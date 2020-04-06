e0803778988050dccce55a1e528e8d590b390427
peterhegman <phegman@gitlab.com>
2020-04-05 21:25:23 -0700

    Fix tests

--- tests/unit/App.spec.ts
+++ tests/unit/App.spec.ts
@@ -4,21 +4,6 @@ import Portfolio from "@/components/Portfolio.vue";
 import ContactForm from "@/components/ContactForm.vue";
 
 describe("App", () => {
-  it("shows contact form when at the `contact` route", () => {
-    const $route = {
-      name: "contact",
-    };
-
-    const wrapper = shallowMount(App, {
-      mocks: {
-        $route,
-      },
-    });
-
-    expect(wrapper.find(ContactForm).isVisible()).toBeTruthy();
-    expect(wrapper.find(Portfolio).isVisible()).toBeFalsy();
-  });
-
   it("shows portfolio when at the `portfolio` route", () => {
     const $route = {
       name: "portfolio",
@@ -31,6 +16,5 @@ describe("App", () => {
     });
 
     expect(wrapper.find(Portfolio).isVisible()).toBeTruthy();
-    expect(wrapper.find(ContactForm).isVisible()).toBeFalsy();
   });
 });
